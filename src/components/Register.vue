bfbd9dbf4effd3cceeba814750f584fea0a9cbff
PatWeid <patrick.weidmueller@gmail.com>
2022-10-04 19:52:36 +0200

    refactor signUp

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,15 +1,33 @@
 <template>
   <v-container>
     <v-row justify="space-around">
-      <v-col  xl="4" lg="4" md="4" sm="12" xs="12">
-        <v-card title="Sign Up">
-          <v-form ref="form">
-            <v-text-field v-model="username" label="Username"></v-text-field>
-            <v-text-field v-model="email" label="Email"></v-text-field>
-            <v-text-field v-model="password" type="password" label="Password"></v-text-field>
-            <v-btn @click="handleRegister">Sign up</v-btn>
-          </v-form>
-        </v-card>
+      <v-col xl="12" lg="12" md="12" sm="12" xs="12">
+        <div class="jumbotron">
+          <h1 class="display-4">Create Account</h1>
+          <p class="lead">With your account you can create a team and join the league.</p>
+          <hr class="my-4">
+
+          <v-card class="bg-blue-grey">
+            <v-container>
+              <v-card-title class="bg-blue-grey text-white text-decoration-underline"><h1>Sign Up</h1></v-card-title>
+            </v-container>
+            <v-form ref="form" v-model="valid">
+              <v-container>
+                <v-text-field v-model="username" :rules="usernameRules" class="bg-white" label="Username"></v-text-field>
+              </v-container>
+              <v-container>
+                <v-text-field v-model="email" :rules="emailRules" class="bg-white" label="Email"></v-text-field>
+              </v-container>
+              <v-container>
+                <v-text-field v-model="password" :rules="passwordRules" class="bg-white" type="password" label="Password"></v-text-field>
+              </v-container>
+              <v-container>
+                <v-btn @click="handleRegister" :disabled="!valid">Sign up</v-btn>
+              </v-container>
+            </v-form>
+          </v-card>
+        </div>
+
       </v-col>
     </v-row>
   </v-container>
@@ -37,7 +55,18 @@ export default {
       email: '',
       password: '',
       role: ['ROLE_USER'],
-    }
+    },
+    usernameRules: [
+      v => !!v || 'Username is required',
+    ],
+    emailRules: [
+      v => !!v || 'Email is required',
+      v => !v || /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+    ],
+    passwordRules: [
+      v => !!v || "Password is required",
+      v => ( v && v.length >= 5 ) || "Password must have at least 5 characters",
+    ],
   }),
   computed: {
     loggedIn() {
